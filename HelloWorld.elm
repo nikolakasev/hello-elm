1a6e8beb388c6d14ecaa219ad29befdca0bd3f1b
Nikola Kasev <nikola.kasev@gmail.com>
2017-05-17 19:04:14 +0200

    formatting with elm-format and work on the API

--- HelloWorld.elm
+++ HelloWorld.elm
@@ -1,6 +1,9 @@
+module Main exposing (..)
+
 import Html exposing (h1, text)
 import Html.Attributes exposing (id)
 
+
 main : Html.Html msg
 main =
-  h1 [id "hw"] [text "Hello World!"]
+    h1 [ id "hw" ] [ text "Hello World!" ]
--- Main.elm
+++ Main.elm
@@ -1,39 +1,53 @@
+module Main exposing (..)
+
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
-main : Program Never Int Msg
-main = Html.beginnerProgram { view = view, model = 0, update = update}
 
-type alias Model = Int
+main : Program Never Int Msg
+main =
+    Html.beginnerProgram { view = view, model = 0, update = update }
+
+
+type alias Model =
+    Int
+
 
 model : Model
-model = 0
+model =
+    0
 
-type Msg = Increment | Decrement
+
+type Msg
+    = Increment
+    | Decrement
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            model + 1
 
-    Decrement ->
-      model - 1
+        Decrement ->
+            model - 1
+
 
 view : Model -> Html Msg
 view model =
-  div []
-    [
-      button [onClick Decrement] [text "-"],
-      -- call tellMeIfNumber by turning the model which is Int into a Maybe first
-      div [] [text (tellMeIfNumber (Just model)) ],
-      button [onClick Increment] [text "+"]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , -- call tellMeIfNumber by turning the model which is Int into a Maybe first
+          div [] [ text (tellMeIfNumber (Just model)) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
+
 
 tellMeIfNumber : Maybe Int -> String
 tellMeIfNumber maybeNumber =
-  case maybeNumber of
-    Nothing ->
-      "Nope, no number"
-    Just number ->
-      "Yup! The number is " ++ toString number
+    case maybeNumber of
+        Nothing ->
+            "Nope, no number"
+
+        Just number ->
+            "Yup! The number is " ++ toString number
